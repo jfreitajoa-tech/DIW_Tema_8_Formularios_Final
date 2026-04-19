916b32975bf4484614831f9fc5d4fa4176a06a6c
jfreitajoa-tech <jfreitajoa@educacion.navarra.es>
2026-04-19 16:40:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/DIW_Tema_8_Formularios_Final/', // Exactamente el nombre de tu repo
+  base: '/DIW_Tema_8_Formularios_Final/', 
   plugins: [react()],
 });
